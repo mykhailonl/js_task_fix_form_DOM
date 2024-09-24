534935c0da75c5837f23a394d966f2b7fc7c2216
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-24 16:10:29 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll("input")
+];
+const formatCamelCase = (str)=>{
+    return str.replace(/([A-Z])/g, " $1").replace(/^./, (string)=>{
+        return string.toUpperCase();
+    }).trim();
+};
+inputs.forEach((input)=>{
+    const labelElem = document.createElement("label");
+    labelElem.className = "field-label";
+    labelElem.htmlFor = input.id;
+    labelElem.textContent = formatCamelCase(input.name);
+    input.insertAdjacentElement("beforebegin", labelElem);
+    input.placeholder = formatCamelCase(input.name);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
